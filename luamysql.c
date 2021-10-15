3385eb39f3b4123e82fc8fb2f449ab1c21245536
Guoyu Ou <benogy@gmail.com>
2021-10-15 20:33:32 +0800

    store metatable of mysqlclient and mysqlresult as upvalues

--- luamysql.c
+++ luamysql.c
@@ -1,24 +1,20 @@
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
 #include <mysql/mysql.h>
-
-static const char* MySQLLib = "MySQL.lib";
-static const char* MySQLResult = "MySQLResult";
 
 /* ------------------------------------------------------------------------- */
 
-#define mysql_init_error(l, fmt, ...) \
-    do { \
-        lua_pushnil(l); /* empty result */ \
+#define mysql_init_error(l, fmt, ...)                               \
+    do {                                                            \
+        lua_pushnil(l); /* empty result */                          \
         lua_pushfstring(l, fmt, ##__VA_ARGS__); /* error message */ \
     } while (0)
 
 /* return 0 if ok */
-static inline int get_args(lua_State* l, const char** host, unsigned int* port,
-                           const char** user, const char** password,
-                           const char** db)
-{
+static int get_args(lua_State* l, const char** host, unsigned int* port,
+                    const char** user, const char** password,
+                    const char** db) {
     int argtype;
 
     if (lua_gettop(l) != 1) {
@@ -88,18 +84,19 @@ static inline int get_args(lua_State* l, const char** host, unsigned int* port,
 }
 
 /* return a connection and an error message */
-static int l_new_mysqlclient(lua_State* l)
-{
+static int l_new_mysqlclient(lua_State* l) {
     MYSQL* conn;
     unsigned int port;
     const char *host, *user = NULL, *password = NULL, *db = NULL;
     const char* errmsg;
 
-    if (get_args(l, &host, &port, &user, &password, &db) != 0)
+    if (get_args(l, &host, &port, &user, &password, &db) != 0) {
         return 2;
+    }
 
     conn = lua_newuserdata(l, sizeof(MYSQL));
-    luaL_setmetatable(l, MySQLLib);
+    lua_pushvalue(l, lua_upvalueindex(1));
+    lua_setmetatable(l, -2);
 
     if (!mysql_init(conn)) {
         errmsg = "mysql_init() failed.";
@@ -123,93 +120,69 @@ conn_err:
 /* ------------------------------------------------------------------------- */
 
 /* return an error message if fails, otherwise nil */
-static int l_mysqlclient_ping(lua_State* l)
-{
-    MYSQL* conn;
-
-    conn = luaL_testudata(l, 1, MySQLLib);
-    if (!conn) {
-        lua_pushstring(l, "argument #1 is not a mysql client.");
-        return 1;
-    }
-
-    if (mysql_ping(conn) == 0)
+static int l_mysqlclient_ping(lua_State* l) {
+    MYSQL* conn = lua_touserdata(l, 1);
+    if (mysql_ping(conn) == 0) {
         lua_pushnil(l);
-    else
+    } else {
         lua_pushstring(l, mysql_error(conn));
-
+    }
     return 1;
 }
 
 /* return an error message if fails, otherwise nil */
-static int l_mysqlclient_selectdb(lua_State* l)
-{
+static int l_mysqlclient_selectdb(lua_State* l) {
     MYSQL* conn;
     const char* db;
-
-    conn = luaL_testudata(l, 1, MySQLLib);
-    if (!conn) {
-        lua_pushstring(l, "argument #1 is not a mysql client.");
-        return 1;
-    }
 
     if (!lua_isstring(l, 2)) {
         lua_pushfstring(l, "argument #2 expects a db name, but given a %s.",
                         lua_typename(l, lua_type(l, 2)));
         return 1;
     }
+
+    conn = lua_touserdata(l, 1);
     db = lua_tostring(l, 2);
 
-    if (mysql_select_db(conn, db) == 0)
+    if (mysql_select_db(conn, db) == 0) {
         lua_pushnil(l);
-    else
+    } else {
         lua_pushstring(l, mysql_error(conn));
+    }
 
     return 1;
 }
 
 /* return an error message if fails, otherwise nil */
-static int l_mysqlclient_setcharset(lua_State* l)
-{
+static int l_mysqlclient_setcharset(lua_State* l) {
     MYSQL* conn;
     const char* charset;
-
-    conn = luaL_testudata(l, 1, MySQLLib);
-    if (!conn) {
-        lua_pushstring(l, "argument #1 is not a mysql client.");
-        return 1;
-    }
 
     if (!lua_isstring(l, 2)) {
         lua_pushfstring(l, "argument #2 expects a charset string, but given a %s.",
                         lua_typename(l, lua_type(l, 2)));
         return 1;
     }
+
+    conn = lua_touserdata(l, 1);
     charset = lua_tostring(l, 2);
 
-    if (mysql_set_character_set(conn, charset) == 0)
+    if (mysql_set_character_set(conn, charset) == 0) {
         lua_pushnil(l);
-    else
+    } else {
         lua_pushstring(l, mysql_error(conn));
+    }
 
     return 1;
 }
 
 /* return the escaped string and the error message */
-static int l_mysqlclient_escape(lua_State* l)
-{
+static int l_mysqlclient_escape(lua_State* l) {
     MYSQL* conn;
     const char* content;
     char* buf;
     luaL_Buffer lbuf;
     unsigned long len;
-
-    conn = luaL_testudata(l, 1, MySQLLib);
-    if (!conn) {
-        lua_pushnil(l);
-        lua_pushstring(l, "argument #1 is not a mysql client.");
-        return 2;
-    }
 
     if (!lua_isstring(l, 2)) {
         int type = lua_type(l, 2);
@@ -233,6 +206,7 @@ static int l_mysqlclient_escape(lua_State* l)
         return 2;
     }
 
+    conn = lua_touserdata(l, 1);
     len = mysql_real_escape_string(conn, buf, content, len);
     if (len == (unsigned long)(-1)) {
         lua_pushnil(l);
@@ -247,19 +221,11 @@ static int l_mysqlclient_escape(lua_State* l)
 }
 
 /* return the result set and the error message */
-static int l_mysqlclient_execute(lua_State* l)
-{
+static int l_mysqlclient_execute(lua_State* l) {
     int err;
     MYSQL* conn;
     const char* sqlstr;
-    unsigned long sqllen;
-
-    conn = luaL_testudata(l, 1, MySQLLib);
-    if (!conn) {
-        lua_pushnil(l);
-        lua_pushstring(l, "argument #1 is not a mysql client.");
-        return 2;
-    }
+    unsigned long sqllen = 0;
 
     if (!lua_isstring(l, 2)) {
         int type = lua_type(l, 2);
@@ -268,87 +234,68 @@ static int l_mysqlclient_execute(lua_State* l)
                         lua_typename(l, type));
         return 2;
     }
-    sqlstr = lua_tolstring(l, 2, &sqllen);
 
+    sqlstr = lua_tolstring(l, 2, &sqllen);
     if (sqllen == 0) {
         lua_pushnil(l);
         lua_pushstring(l, "invalid SQL statement.");
         return 2;
     }
 
+    conn = lua_touserdata(l, 1);
     err = mysql_real_query(conn, sqlstr, sqllen);
     if (err) {
         lua_pushnil(l);
         lua_pushstring(l, mysql_error(conn));
     } else {
         MYSQL_RES** result = lua_newuserdata(l, sizeof(MYSQL_RES*));
-        luaL_setmetatable(l, MySQLResult);
-
         *result = mysql_store_result(conn);
+        lua_pushvalue(l, lua_upvalueindex(1));
+        lua_setmetatable(l, -2);
         lua_pushnil(l);
     }
 
     return 2;
 }
 
-static int l_mysqlclient_gc(lua_State* l)
-{
-    MYSQL* conn = luaL_testudata(l, 1, MySQLLib);
-    if (conn)
-        mysql_close(conn);
-
+static int l_mysqlclient_gc(lua_State* l) {
+    mysql_close(lua_touserdata(l, 1));
     return 0;
 }
 
 /* ------------------------------------------------------------------------- */
 
 /* return the fieldnamelist, or nil if fails. */
-static int l_mysqlresult_fieldnamelist(lua_State* l)
-{
-    MYSQL_RES** result = luaL_testudata(l, 1, MySQLResult);
+static int l_mysqlresult_fieldnamelist(lua_State* l) {
+    int i;
+    MYSQL_RES** result = lua_touserdata(l, 1);
+    int nr_field = mysql_num_fields(*result);
+    MYSQL_FIELD* fieldlist = mysql_fetch_fields(*result);
 
-    if (!result)
-        lua_pushnil(l);
-    else {
-        int i;
-        int nr_field = mysql_num_fields(*result);
-        MYSQL_FIELD* fieldlist = mysql_fetch_fields(*result);
-
-        lua_newtable(l);
-        for (i = 0; i < nr_field; ++i) {
-            lua_pushstring(l, fieldlist[i].name);
-            lua_rawseti(l, -2, i + 1);
-        }
+    lua_createtable(l, nr_field, 0);
+    for (i = 0; i < nr_field; ++i) {
+        lua_pushstring(l, fieldlist[i].name);
+        lua_rawseti(l, -2, i + 1);
     }
-
     return 1;
 }
 
 /* return the number of record(s), or nil if fails. */
-static int l_mysqlresult_size(lua_State* l)
-{
-    MYSQL_RES** result = luaL_testudata(l, 1, MySQLResult);
-
-    if (!result)
-        lua_pushnil(l);
-    else
-        lua_pushinteger(l, mysql_num_rows(*result));
-
+static int l_mysqlresult_size(lua_State* l) {
+    MYSQL_RES** result = lua_touserdata(l, 1);
+    lua_pushinteger(l, mysql_num_rows(*result));
     return 1;
 }
 
-static int l_mysqlresult_record_iter(lua_State* l)
-{
-    MYSQL_ROW row;
+static int l_mysqlresult_record_iter(lua_State* l) {
     MYSQL_RES** result = lua_touserdata(l, lua_upvalueindex(1));
-    int nr_field = lua_tointeger(l, lua_upvalueindex(2));
-
-    row = mysql_fetch_row(*result);
+    MYSQL_ROW row = mysql_fetch_row(*result);
     if (row) {
         int i;
         unsigned long* lengths = mysql_fetch_lengths(*result);
+        int nr_field = lua_tointeger(l, lua_upvalueindex(2));
 
-        lua_newtable(l);
+        lua_createtable(l, nr_field, 0);
         for (i = 0; i < nr_field; ++i) {
             lua_pushlstring(l, row[i], lengths[i]);
             lua_rawseti(l, -2, i + 1);
@@ -356,75 +303,75 @@ static int l_mysqlresult_record_iter(lua_State* l)
 
         return 1;
     }
-
     return 0;
 }
 
 /* return a record iterator, or nil if fails. */
-static int l_mysqlresult_recordlist(lua_State* l)
-{
-    MYSQL_RES** result = luaL_testudata(l, 1, MySQLResult);
-
-    if (!result)
-        lua_pushnil(l);
-    else {
-        lua_pushvalue(l, -1); /* duplicate the result */
-        lua_pushinteger(l, mysql_num_fields(*result)); /* number of fields */
-        lua_pushcclosure(l, l_mysqlresult_record_iter, 2);
-    }
-
+static int l_mysqlresult_recordlist(lua_State* l) {
+    MYSQL_RES** result = lua_touserdata(l, 1);
+    lua_pushvalue(l, 1);
+    lua_pushinteger(l, mysql_num_fields(*result));
+    lua_pushcclosure(l, l_mysqlresult_record_iter, 2);
     return 1;
 }
 
-static int l_mysqlresult_gc(lua_State* l)
-{
-    MYSQL_RES** result = luaL_testudata(l, 1, MySQLResult);
-
-    if (*result)
-        mysql_free_result(*result);
-
+static int l_mysqlresult_gc(lua_State* l) {
+    MYSQL_RES** result = lua_touserdata(l, 1);
+    mysql_free_result(*result);
     return 0;
 }
 
 /* ------------------------------------------------------------------------- */
 
-static const struct luaL_Reg mysqlclient_f[] = {
-    {"newclient", l_new_mysqlclient},
-    {NULL, NULL},
-};
+static void create_mysqlresult_funcs(lua_State* l) {
+    lua_newtable(l);
 
-static const struct luaL_Reg mysqlclient_m[] = {
-    {"ping", l_mysqlclient_ping},
-    {"selectdb", l_mysqlclient_selectdb},
-    {"setcharset", l_mysqlclient_setcharset},
-    {"escape", l_mysqlclient_escape},
-    {"execute", l_mysqlclient_execute},
-    {"__gc", l_mysqlclient_gc},
-    {NULL, NULL},
-};
-
-static const struct luaL_Reg mysqlresult_lib[] = {
-    {"size", l_mysqlresult_size},
-    {"fieldnamelist", l_mysqlresult_fieldnamelist},
-    {"recordlist", l_mysqlresult_recordlist},
-    {"__gc", l_mysqlresult_gc},
-    {NULL, NULL},
-};
-
-int luaopen_luamysql(lua_State* l)
-{
-    /* meta table for mysql client */
-    luaL_newmetatable(l, MySQLLib);
     lua_pushvalue(l, -1);
     lua_setfield(l, -2, "__index");
-    luaL_setfuncs(l, mysqlclient_m, 0);
 
-    /* meta table for mysql result */
-    luaL_newmetatable(l, MySQLResult);
+    lua_pushcfunction(l, l_mysqlresult_size);
+    lua_setfield(l, -2, "size");
+
+    lua_pushcfunction(l, l_mysqlresult_fieldnamelist);
+    lua_setfield(l, -2, "fieldnamelist");
+
+    lua_pushcfunction(l, l_mysqlresult_recordlist);
+    lua_setfield(l, -2, "recordlist");
+
+    lua_pushcfunction(l, l_mysqlresult_gc);
+    lua_setfield(l, -2, "__gc");
+}
+
+static void create_mysqlclient_funcs(lua_State* l) {
+    lua_newtable(l);
+
     lua_pushvalue(l, -1);
     lua_setfield(l, -2, "__index");
-    luaL_setfuncs(l, mysqlresult_lib, 0);
 
-    luaL_newlib(l, mysqlclient_f);
+    lua_pushcfunction(l, l_mysqlclient_ping);
+    lua_setfield(l, -2, "ping");
+
+    lua_pushcfunction(l, l_mysqlclient_selectdb);
+    lua_setfield(l, -2, "selectdb");
+
+    lua_pushcfunction(l, l_mysqlclient_setcharset);
+    lua_setfield(l, -2, "setcharset");
+
+    lua_pushcfunction(l, l_mysqlclient_escape);
+    lua_setfield(l, -2, "escape");
+
+    create_mysqlresult_funcs(l);
+    lua_pushcclosure(l, l_mysqlclient_execute, 1);
+    lua_setfield(l, -2, "execute");
+
+    lua_pushcfunction(l, l_mysqlclient_gc);
+    lua_setfield(l, -2, "__gc");
+}
+
+int luaopen_luamysql(lua_State* l) {
+    lua_createtable(l, 0, 1);
+    create_mysqlclient_funcs(l);
+    lua_pushcclosure(l, l_new_mysqlclient, 1);
+    lua_setfield(l, -2, "newclient");
     return 1;
 }
